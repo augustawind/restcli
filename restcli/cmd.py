fb638a9d26ebaf2e5d57f3deebd5fe89f97cc826
Dustin Rohde <dustin.rohde@gmail.com>
2017-03-12 18:32:42 -0400

    Organize cmd.py.

--- restcli/cmd.py
+++ restcli/cmd.py
@@ -9,9 +9,27 @@ class Cmd(cmd.Cmd):
         super().__init__()
         self.app = App(groups_file, env_file, self.stdout)
 
-    def do_save(self, arg):
-        """Save the current environment to disk."""
-        self.app.save()
+    def do_run(self, arg):
+        """Run an HTTP request."""
+        args = arg.split()
+        if len(args) != 2:
+            return self.app.usage('run', 'Invalid input.')
+
+        self.app.run(*args)
+
+    def do_inspect(self, arg):
+        """Inspect a Group, Request, or Attribute."""
+        args = arg.split()
+        if len(args) == 0:
+            self.app.usage('inspect', 'Invalid input.')
+        if len(args) > 3:
+            self.app.usage('inspect', 'Invalid input.')
+
+        self.inspect(*args)
+
+    def do_env(self, arg):
+        """Display the current environment."""
+        self.app.print_env()
 
     def do_reload(self, arg):
         "Reload the Collection and/or Environment from disk."
@@ -25,15 +43,9 @@ class Cmd(cmd.Cmd):
         if 'env' in files:
             self.app.r.load_env()
 
-    def do_set_env(self, arg):
-        """Set and load a new Environment file."""
-        args = arg.split()
-        if len(args) != 1:
-            self.app.usage('set_env', 'Missing argument')
-            return
-
-        path = args[0]
-        self.app.r.load_env(path)
+    def do_save(self, arg):
+        """Save the current environment to disk."""
+        self.app.save()
 
     def do_set_collection(self, arg):
         args = arg.split()
@@ -44,24 +56,12 @@ class Cmd(cmd.Cmd):
         path = args[0]
         self.app.r.load_collection(path)
 
-    def do_inspect(self, arg):
-        """Inspect a Group, Request, or Attribute."""
+    def do_set_env(self, arg):
+        """Set and load a new Environment file."""
         args = arg.split()
-        if len(args) == 0:
-            self.app.usage('inspect', 'Invalid input.')
-        if len(args) > 3:
-            self.app.usage('inspect', 'Invalid input.')
+        if len(args) != 1:
+            self.app.usage('set_env', 'Missing argument')
+            return
 
-        self.inspect(*args)
-
-    def do_run(self, arg):
-        """Run an HTTP request."""
-        args = arg.split()
-        if len(args) != 2:
-            return self.app.usage('run', 'Invalid input.')
-
-        self.app.run(*args)
-
-    def do_env(self, arg):
-        """Display the current environment."""
-        self.app.print_env()
+        path = args[0]
+        self.app.r.load_env(path)
