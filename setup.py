e43f6f423e6a7b75ecd981333506df47647c4154
Dustin Rohde <dustin.rohde@gmail.com>
2017-03-15 13:36:06 -0400

    Install prompt_toolkit

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     install_requires=[
         'click>=6,<7',
         'jinja2>=2,<3',
+        'prompt_toolkit>=1,<2',
         'Pygments>=2,<3',
         'PyYAML>=3,<4',
         'requests>=2,<3',
