bac56afca01ab052f18ddff81dbf344bf5d84900
Dustin Rohde <dustin.rohde@gmail.com>
2017-04-10 16:24:07 -0400

    Remove ignore_missing_* args from set_env contrib script.

--- restcli/contrib/scripts.py
+++ restcli/contrib/scripts.py
@@ -1,20 +1,15 @@
 def define(response, env, *args, **kwargs):
 
-    def set_env(status, var, path, ignore_missing_index=False,
-                ignore_missing_key=False):
+    def set_env(status, var, path):
         """Shortcut for checking the status code and setting an env var."""
         if response.status_code == status:
             value = response.json()
 
-            for key in path:
-                try:
+            try:
+                for key in path:
                     value = value[key]
-                except IndexError as exc:
-                    if not ignore_missing_index:
-                        raise exc
-                except KeyError as exc:
-                    if not ignore_missing_key:
-                        raise exc
+            except (IndexError, KeyError):
+                return
             env[var] = value
 
     return locals()
