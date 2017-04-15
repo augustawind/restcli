247602b41a6ed679b7d7fc09ae3208c8a4c0d344
Dustin Rohde <dustin.rohde@gmail.com>
2017-04-15 14:50:26 -0400

    Add tests for parse_json_field().

--- tests/test_parser/test_parser.py
+++ tests/test_parser/test_parser.py
@@ -5,12 +5,12 @@ import pytest
 
 from restcli.parser import lexer, parser
 
-from ..helpers import get_random_unicode
+from ..helpers import get_random_ascii, get_random_unicode
 
 
 class TestParseURLParam:
 
-    def test_parse_url_param(self):
+    def test_valid(self):
         action = get_random_action()
         key = ''.join(random.sample(parser.VALID_URL_CHARS, 10))
         value = ''.join(random.sample(parser.VALID_URL_CHARS, 10))
@@ -22,7 +22,7 @@ class TestParseURLParam:
         ))
         assert result == expected
 
-    def test_parse_url_param_invalid(self):
+    def test_invalid(self):
         action = get_random_action()
         key = get_random_unicode(10)
         value = get_random_unicode(10)
@@ -30,11 +30,76 @@ class TestParseURLParam:
             parser.parse_url_param(action, key, value)
 
 
-def test_parse_json_field():
-    action = get_random_action()
-    key = 'foo'
-    value = 'true'
-    result = parser.parse_json_field(action, key, value)
+class TestParseJSONField:
+
+    @staticmethod
+    def run_test(in_val, out_val, key=None, out_key=None):
+        action = get_random_action()
+        key = out_key or key or get_random_ascii(11)
+        result = parser.parse_json_field(action, key, in_val)
+        expected = OrderedDict((
+            (key, OrderedDict((
+                (action, out_val),
+            ))),
+        ))
+        from pprint import pformat
+        print('ACTUAL ==>', pformat(result))
+        print('EXPECT ==>', pformat(expected))
+        assert result == expected
+
+    def test_bool(self):
+        self.run_test(
+            in_val='true',
+            out_val=True,
+        )
+
+    def test_number_int(self):
+        self.run_test(
+            in_val='11',
+            out_val=11,
+        )
+
+    def test_number_float(self):
+        self.run_test(
+            in_val='26.5',
+            out_val=26.5,
+        )
+
+    def test_null(self):
+        self.run_test(
+            in_val='null',
+            out_val=None,
+        )
+
+    def test_array(self):
+        self.run_test(
+            in_val='[1, 2, 3]',
+            out_val=[1, 2, 3],
+        )
+
+    def test_object(self):
+        self.run_test(
+            in_val='{"foo": "bar", "baz": "biff"}',
+            out_val={'foo': 'bar', 'baz': 'biff'},
+        )
+
+    def test_compound_1(self):
+        self.run_test(
+            in_val='[5, 5.25, "hello", true, null, [1, 2], {"abc": "def"}]',
+            out_val=[5, 5.25, 'hello', True, None, [1, 2], {'abc': 'def'}],
+        )
+
+    def test_compound_2(self):
+        self.run_test(
+            in_val=(
+                '{"who": null, "whom": true, "whomst": ["x", "y", "z"],'
+                ' "whomst\'d\'ve": {"x": 11, "y": [2, 2], "z": [0, [], {}]}}'
+            ),
+            out_val=(
+                {'who': None, 'whom': True, 'whomst': ['x', 'y', 'z'],
+                 "whomst'd've": {'x': 11, 'y': [2, 2], 'z': [0, [], {}]}}
+            ),
+        )
 
 
 def get_random_action():
