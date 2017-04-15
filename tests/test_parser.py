ab6affd51e2ee60da15bd6563ea1b18572b08232
Dustin Rohde <dustin.rohde@gmail.com>
2017-04-15 06:39:07 -0400

    Write test for is_ascii.

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,6 +1,38 @@
+import random
+import string
 from collections import OrderedDict
 
 from restcli import parser
+
+
+def get_random_unicode(length):
+    """Helper to generate a random Unicode string."""
+    include_ranges = [
+        ( 0x0021, 0x0021 ),
+        ( 0x0023, 0x0026 ),
+        ( 0x0028, 0x007E ),
+        ( 0x00A1, 0x00AC ),
+        ( 0x00AE, 0x00FF ),
+        ( 0x0100, 0x017F ),
+        ( 0x0180, 0x024F ),
+        ( 0x2C60, 0x2C7F ),
+        ( 0x16A0, 0x16F0 ),
+        ( 0x0370, 0x0377 ),
+        ( 0x037A, 0x037E ),
+        ( 0x0384, 0x038A ),
+        ( 0x038C, 0x038C ),
+    ]
+    alphabet = [
+        chr(code_point)
+        for current_range in include_ranges
+        for code_point in range(current_range[0], current_range[1] + 1)
+    ]
+    return ''.join(random.sample(alphabet, length))
+
+
+def get_random_ascii(length):
+    """Helper to generate a random ASCII string."""
+    return ''.join(random.sample(string.printable, length))
 
 
 class TestMiscUtilities:
@@ -30,3 +62,12 @@ class TestMiscUtilities:
             ('baz', [5]),
         ))
         assert d0 == expected
+
+    def test_is_ascii(self):
+        assert parser.is_ascii(get_random_unicode(50)) is False
+        assert parser.is_ascii(get_random_unicode(50)) is False
+        assert parser.is_ascii(get_random_unicode(50)) is False
+
+        assert parser.is_ascii(get_random_ascii(50)) is True
+        assert parser.is_ascii(get_random_ascii(50)) is True
+        assert parser.is_ascii(get_random_ascii(50)) is True
