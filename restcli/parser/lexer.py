e2f434108a565e8a81052d45b49bcc56eeec8950
Dustin Rohde <dustin.rohde@gmail.com>
2017-05-12 07:37:36 -0400

    Wrote a bunch more tests.

--- restcli/parser/lexer.py
+++ restcli/parser/lexer.py
@@ -6,15 +6,14 @@ from restcli.utils import split_quoted
 
 
 class ACTIONS(enum.Enum):
-    """Actions that can be applied to tokens."""
+    append = 'append'
     assign = 'assign'
     delete = 'delete'
-    append = 'append'
 
 
-lexer = argparse.ArgumentParser(add_help=False)
-lexer.add_argument('-d', '--{}'.format(ACTIONS.delete.name), action='append')
-lexer.add_argument('-a', '--{}'.format(ACTIONS.append.name), action='append')
+lexer = argparse.ArgumentParser(prog='lexer', add_help=False)
+lexer.add_argument('-d', '--{}'.format(ACTIONS.delete.value), action='append')
+lexer.add_argument('-a', '--{}'.format(ACTIONS.append.value), action='append')
 
 Node = namedtuple('Node', ['action', 'token'])
 
@@ -23,10 +22,12 @@ def lex(argument_str):
     """Lex a string into a sequence of (action, tokens) pairs."""
     argv = split_quoted(argument_str)
     opts, args = lexer.parse_known_args(argv)
+    from pprint import pformat
     tokens = OrderedDict(
-        Node(action=ACTIONS[k], token=v)
+        Node(action=k, token=v)
         for k, v in vars(opts).items()
         if v is not None
     )
-    tokens[ACTIONS.assign] = split_quoted(' '.join(args))
+    if args:
+        tokens[ACTIONS.assign.name] = split_quoted(' '.join(args))
     return tuple(tokens.items())
--- tests/test_parser/test_lexer.py
+++ tests/test_parser/test_lexer.py
@@ -1,10 +1,53 @@
 from restcli.parser import lexer
+from tests.helpers import contents_equal
 
-from ..helpers import contents_equal
+
+class LexerTestMixin:
+
+    action = NotImplemented
+
+    def transform_args(self, arg):
+        raise NotImplementedError
+
+    def assert_lex(self, args, result):
+        arg = ' '.join(self.transform_args(args))
+        tokens = lexer.lex(arg)
+        assert contents_equal(tokens, [
+            (self.action, result),
+        ])
+
+    def test_1_header_single_quotes(self):
+        args = ["Authorization:'JWT abc123.foo'"]
+        result = ["Authorization:'JWT abc123.foo'"]
+        self.assert_lex(args, result)
+
+    def test_2_json_str_double_quotes(self):
+        args = ['description="foo bar baz"']
+        result = ['description="foo bar baz"']
+        self.assert_lex(args, result)
+
+    def test_3_nested_json_number_unquoted(self):
+        args = ['.location.postal_code:=12345']
+        result = ['.location.postal_code:=12345']
+        self.assert_lex(args, result)
+
+    def test_4(self):
+        args = ['foo:bar', 'conditions[0].var="haha ha"']
+        result = ['foo:bar', 'conditions[0].var="haha ha"']
+        self.assert_lex(args, result)
 
 
-def test_assign_headers():
-    arg = "Authorization:'JWT abc123.foo'"
-    tokens = lexer.lex(arg)
-    expected = [(lexer.ACTIONS.assign, ["Authorization:'JWT abc123.foo'"])]
-    assert contents_equal(tokens, expected)
+class TestAssign(LexerTestMixin):
+
+    action = lexer.ACTIONS.assign.value
+
+    def transform_args(self, args):
+        return args
+
+
+class TestAppend(LexerTestMixin):
+
+    action = lexer.ACTIONS.append.value
+
+    def transform_args(self, args):
+        return ['-a {}'.format(arg) for arg in args]
