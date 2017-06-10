9f52ff6f256668bcf3775401c7a89c65a13863b5
Dustin Rohde <dustin.rohde@gmail.com>
2017-06-10 07:23:19 -0400

    Refactor AttrMap and AttrSeq data types.

--- restcli/utils.py
+++ restcli/utils.py
@@ -4,18 +4,20 @@ import six
 
 
 class AttrSeq(Sequence):
-    """An immutable sequence that supports dot and bracket notation."""
+    """An immutable sequence that supports dot notation.
+    
+    Args:
+        *args: Items to create the sequence from.
+    """
 
     def __init__(self, *args):
-        self._dict = {x: x for x in args}
-
-    def __copy__(self):
-        return self.__class__(*self._dict.keys())
+        self._dict = OrderedDict((x, x) for x in args)
 
     def __getattr__(self, item):
         return self._dict[item]
 
-    __getitem__ = __getattr__
+    def __getitem__(self, item):
+        return self._dict[item]
 
     def __iter__(self):
         return iter(self._dict)
@@ -23,30 +25,54 @@ class AttrSeq(Sequence):
     def __len__(self):
         return len(self._dict)
 
+    def __copy__(self):
+        return type(self)(*six.iterkeys(self._dict))
+
+    copy = __copy__
+
 
 class AttrMap(Mapping):
-    """An immutable, ordered mapping that supports dot and bracket notation.
+    """An immutable, ordered mapping that supports dot notation.
     
-    A ``name`` attr is also added to each value which returns the key of that
-    value.
+    Args:
+        *pairs: 2-tuples to create the mapping from.
     """
 
     def __init__(self, *pairs):
         self._dict = OrderedDict(pairs)
 
-    def __copy__(self):
-        return self.__class__(*self._dict.items())
-
-    def __getattr__(self, item):
+    def __getitem__(self, item):
         return self._dict[item]
 
-    __getitem__ = __getattr__
+    __getattr__ = __getitem__
 
     def __iter__(self):
         return iter(self._dict)
 
     def __len__(self):
         return len(self._dict)
+
+    def __copy__(self):
+        return type(self)(*self._dict.items())
+
+    copy = __copy__
+
+
+class MultiAttrMap(AttrMap):
+    """Like AttrMap, but supports multiple keys for each value.
+    
+    Each key must be a tuple. When accessing attrs, any key in the tuple works.
+    """
+
+    def __init__(self, *pairs):
+        expanded_pairs = []
+        for multi_key, value in six.iteritems(pairs):
+            if not isinstance(multi_key, tuple):
+                raise TypeError("'%s' object is not an instance of tuple"
+                                % type(multi_key))
+            expanded_pairs.extend((key, value) for key in multi_key)
+
+        super(MultiAttrMap, self).__init__(*expanded_pairs)
 
 
 def recursive_update(mapping, data):
