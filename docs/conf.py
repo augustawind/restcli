1cdcb67b5f2d831e4cb0996a34b39fdcbd2ee8d4
Dustin Rohde <dustin.rohde@gmail.com>
2018-01-30 21:07:11 -0500

    docs: enable autodoc extension

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,8 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.todo',
-    'sphinx.ext.githubpages']
+              'sphinx.ext.githubpages',
+              'sphinx.ext.autodoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
