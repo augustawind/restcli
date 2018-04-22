c065c5488eecd6130c602aaef3916b7d8d462e14
Dustin Rohde <dustin.rohde@gmail.com>
2018-04-22 07:38:25 -0400

    feat: add to simple example

---
+++ examples/simple/config.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+#
+# simple.sh
+# Source this file to configure restcli for the example.
+#
+export RESTCLI_COLLECTION="./simple.collection.yaml"
+export RESTCLI_ENV="./simple.env.yaml"
+export RESTCLI_AUTOSAVE="1"
