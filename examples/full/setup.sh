ad09c1b89da10bf8162f8a83ba4cff99a4721490
Dustin Rohde <dustin.rohde@gmail.com>
2019-12-05 02:58:56 -0500

    add "full"  example

---
+++ examples/full/setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+#
+# setup.sh
+# Source this file from the package root to configure restcli for the example.
+#
+export RESTCLI_COLLECTION="./examples/full/collection.yaml"
+export RESTCLI_ENV="./examples/full/env.yaml"
+export RESTCLI_AUTOSAVE="1"
