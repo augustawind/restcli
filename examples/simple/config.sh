3b5aaa17376707f3d2b6ac8cd6f29cfdf3d99526
Dustin Rohde <dustin.rohde@gmail.com>
2019-12-05 00:43:18 -0500

    rename simple example files

--- examples/simple/config.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-#
-# simple.sh
-# Source this file to configure restcli for the example.
-#
-export RESTCLI_COLLECTION="./simple.collection.yaml"
-export RESTCLI_ENV="./simple.env.yaml"
-export RESTCLI_AUTOSAVE="1"
---
+++ examples/simple/setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+#
+# setup.sh
+# Source this file from the package root to configure restcli for the example.
+#
+export RESTCLI_COLLECTION="./examples/simple/collection.yaml"
+export RESTCLI_ENV="./examples/simple/env.yaml"
+export RESTCLI_AUTOSAVE="1"
