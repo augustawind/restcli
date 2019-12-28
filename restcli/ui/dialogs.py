7b37e673b298cb2d1ba12d4f97add8c37ac189a5
Dustin Rohde <dustin.rohde@gmail.com>
2019-12-27 21:38:27 -0500

    add confirmation for existing files on export

--- restcli/ui/dialogs.py
+++ restcli/ui/dialogs.py
@@ -1,11 +1,21 @@
 from __future__ import annotations
 
 import abc
+import asyncio
 from asyncio import Future
-from typing import TYPE_CHECKING
+from contextlib import asynccontextmanager
+from functools import partial
+from pathlib import Path
+from typing import TYPE_CHECKING, Callable
 
 from prompt_toolkit.completion.filesystem import PathCompleter
-from prompt_toolkit.layout.containers import Float, HSplit
+from prompt_toolkit.filters import Condition
+from prompt_toolkit.layout.containers import (
+    AnyContainer,
+    ConditionalContainer,
+    Float,
+    HSplit,
+)
 from prompt_toolkit.layout.dimension import D
 from prompt_toolkit.widgets import Button, Dialog, Label, RadioList, TextArea
 
@@ -15,29 +25,76 @@ from restcli.workspace import Collection, Environment
 if TYPE_CHECKING:
     from restcli.ui import UI
 
-OK_TEXT = "OK"
-CANCEL_TEXT = "Cancel"
+TEXT_OK = "OK"
+TEXT_CANCEL = "Cancel"
 
 
-class BaseFileDialog(Dialog, metaclass=abc.ABCMeta):
-    def __init__(self, ui: UI, title: str, text: str):
+class AsyncDialog(Dialog):
+    """Base class for floating Dialogs that await a result from a Future."""
+
+    def __init__(self, ui: UI, title: str, body: AnyContainer, **kwargs):
         self.future = Future()
         self.ui = ui
+        self.float_ = None
 
-        self.ok_button = Button(OK_TEXT, self.handle_ok)
-        self.cancel_button = Button(CANCEL_TEXT, self.handle_cancel)
+        super().__init__(title=title, body=body, **kwargs)
+
+    async def run(self):
+        async with self.render():
+            self.ui.app.layout.focus(self)
+            result = await self.future
+            self.ui.app.layout.focus_last()
+
+        return result
+
+    async def run_from_dialog(self, dialog: AsyncDialog):
+        self.ui.root_container.floats.remove(dialog.float_)
+
+        async with self.render():
+            self.ui.app.layout.focus(self)
+            result = await self.future
+            self.ui.app.layout.focus(dialog)
+
+        return result
+
+    @asynccontextmanager
+    async def render(self):
+        self.float_ = Float(self)
+        self.ui.root_container.floats.insert(0, self.float_)
+
+        yield
+
+        if self.float_ in self.ui.root_container.floats:
+            self.ui.root_container.floats.remove(self.float_)
+
+
+class BaseTextInputDialog(AsyncDialog, metaclass=abc.ABCMeta):
+    """Abstract base class for Dialogs that prompt the user for text."""
+
+    def __init__(self, ui: UI, title: str, text: str):
+        self.ok_button = Button(TEXT_OK, self.handle_ok)
+        self.cancel_button = Button(TEXT_CANCEL, self.handle_cancel)
 
         self.text_area = TextArea(
             multiline=False,
             completer=PathCompleter(),
             accept_handler=self.handle_accept_text,
         )
+        self.message_box = TextArea(read_only=True, style="color:#ff0000")
 
         # noinspection PyTypeChecker
         super().__init__(
+            ui=ui,
             title=title,
             body=HSplit(
-                [Label(text=text, dont_extend_height=True), self.text_area],
+                [
+                    Label(text=text, dont_extend_height=True),
+                    self.text_area,
+                    ConditionalContainer(
+                        self.message_box,
+                        Condition(lambda: bool(self.message_box.text)),
+                    ),
+                ],
                 width=D(preferred=80),
                 padding=D(preferred=1, max=1),
             ),
@@ -46,12 +103,15 @@ class BaseFileDialog(Dialog, metaclass=abc.ABCMeta):
             with_background=True,
         )
 
-    @abc.abstractmethod
+    def set_message(self, text: str):
+        self.message_box.text = text
+
     def handle_ok(self):
         """What to do when the OK button is pressed.
 
-        This should perform any validation required, and if successful it
-        should call `self.future.set_result` with the result of the Dialog.
+        This should perform any validation required, and if successful
+        it should call `self.future.set_result` with the result of the
+        Dialog.
         """
 
     def handle_cancel(self):
@@ -62,22 +122,9 @@ class BaseFileDialog(Dialog, metaclass=abc.ABCMeta):
         buf.complete_state = None
         return True
 
-    async def run(self):
-        float_ = Float(self)
-        self.ui.root_container.floats.insert(0, float_)
 
-        self.ui.app.layout.focus(self)
-        result = await self.future
-        self.ui.app.layout.focus_last()
-
-        if float_ in self.ui.root_container.floats:
-            self.ui.root_container.floats.remove(float_)
-
-        return result
-
-
-class BaseRadioFileDialog(BaseFileDialog, metaclass=abc.ABCMeta):
-    """Abstract base class for dialogs that open files."""
+class BaseDocumentInputDialog(BaseTextInputDialog, metaclass=abc.ABCMeta):
+    """Adds a Document-type prompt to BaseTextInputDialog."""
 
     def __init__(self, ui: UI, title: str, text: str):
         super().__init__(ui, title, text)
@@ -100,6 +147,10 @@ class BaseRadioFileDialog(BaseFileDialog, metaclass=abc.ABCMeta):
                     [
                         Label(text=text, dont_extend_height=True),
                         self.text_area,
+                        ConditionalContainer(
+                            self.message_box,
+                            Condition(lambda: bool(self.message_box.text)),
+                        ),
                     ],
                     padding=D(preferred=1, max=1),
                 ),
@@ -110,10 +161,10 @@ class BaseRadioFileDialog(BaseFileDialog, metaclass=abc.ABCMeta):
 
     @abc.abstractmethod
     def handle_ok(self):
-        """Same as :method:`BaseFileDialog.handle_ok`."""
+        """Same as :method:`BaseTextInputDialog.handle_ok`."""
 
 
-class NewFileDialog(BaseRadioFileDialog):
+class NewFileDialog(BaseDocumentInputDialog):
     def __init__(self, ui: UI):
         super().__init__(ui, title="New File", text="File name")
 
@@ -126,7 +177,7 @@ class NewFileDialog(BaseRadioFileDialog):
         self.future.set_result(result)
 
 
-class OpenFileDialog(BaseRadioFileDialog):
+class OpenFileDialog(BaseDocumentInputDialog):
     def __init__(self, ui: UI):
         super().__init__(ui, title="Open File", text="Path to open file")
         self.text_area.text = "collection.yaml"
@@ -139,7 +190,7 @@ class OpenFileDialog(BaseRadioFileDialog):
         self.future.set_result(document_cls(source))
 
 
-class ExportFileDialog(BaseFileDialog):
+class ExportFileDialog(BaseTextInputDialog):
     def __init__(self, ui: UI, title="Export File"):
         super().__init__(ui, title=title, text="Path to save file")
 
@@ -150,9 +201,62 @@ class ExportFileDialog(BaseFileDialog):
             group = self.ui.editor.collection.setdefault(group_name, {})
             group.update(group_data)
 
-        # Save the Collection to the given path
-        path = self.text_area.text.strip()
+        path = Path(self.text_area.text.strip())
+
+        # If existing directory, show error message
+        if path.is_dir():
+            self.set_message(f'Invalid path: "{path.name}" is a directory')
+            return
+
+        # If existing file, run confirmation dialog
+        if path.is_file():
+            confirmation = ConfirmationDialog(
+                self.ui,
+                title="Overwrite file",
+                text=(
+                    f'"{path.name}" already exists.'
+                    " Do you want to replace it?"
+                ),
+            )
+            task = asyncio.create_task(confirmation.run_from_dialog(self))
+
+            def do_if_confirmed(confirmed):
+                if confirmed.result():
+                    self.do_export(path)
+
+            task.add_done_callback(do_if_confirmed)
+        else:
+            self.do_export(path)
+
+    def do_export(self, path: Path):
+        """Save the Collection to the given path."""
         self.ui.editor.collection.source = path
         self.ui.editor.collection.save()
-
         self.future.set_result(path)
+
+
+class ConfirmationDialog(AsyncDialog):
+    def __init__(
+        self,
+        ui: UI,
+        title: str,
+        text: str,
+        ok_text: str = TEXT_OK,
+        cancel_text: str = TEXT_CANCEL,
+    ):
+        super().__init__(
+            ui=ui,
+            title=title,
+            body=Label(text=text, dont_extend_height=True),
+            buttons=[
+                Button(text=ok_text, handler=self.handle_ok),
+                Button(text=cancel_text, handler=self.handle_cancel),
+            ],
+            with_background=True,
+        )
+
+    def handle_ok(self):
+        self.future.set_result(True)
+
+    def handle_cancel(self):
+        self.future.set_result(False)
